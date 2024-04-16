ba74c9ea2ce2d91493c2279c73caaf84f076fef7
brycebien <brycebien@gmail.com>
2024-04-15 21:03:15 -0400

    added tests for feature 2

--- __tests__/bryceUnitTests.test.js
+++ __tests__/bryceUnitTests.test.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const { app, server, main } = require('../index.js');
 
 const todoTask = require('../models/TodoTask');
+const Users = require("../models/UserData");
+
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -121,9 +123,51 @@ describe('GET /subtaskRemove', () => {
         //check the correct status code was given
         expect(res.statusCode).toBe(302);
     });
+});
+
+describe('Password hashing', () => {
+    it('hashes a password', async () => {
+        let user = new Users();
+        user.username = "test";
+        user.setPassword("test");
+        expect(user.hash).toBeDefined();
+        expect(user.salt).toBeDefined();
+        expect(user.hash).not.toEqual("test");
+    });
+});
+
+describe('Password validation', () => {
+    it('validates a password', async () => {
+        let user = new Users();
+        user.username = "test";
+        user.setPassword("test");
+        expect(user.validatePassword("test")).toBe(true);
+        expect(user.validatePassword("wrong")).toBe(false);
+    });
+});
+
+describe('Storing user data', () => {
+    it('stores user data securely', async () => {
+        await main();
+        const res = await request(app)
+            .post('/create-account')
+            .send({
+                username: 'test',
+                password: 'test'
+            })
+            .expect(200);
+        expect(res.body.success).toBe(true);
+        
+        const user = await Users.findOne({ username: 'test' });
+        expect(user).toBeDefined();
+        expect(user.username).toBe('test');
+        expect(user.hash).toBeDefined();
+        expect(user.salt).toBeDefined();
+        expect(user.hash).not.toEqual('test');
+    });
 
     afterAll(async () => {    
         await server.close();
         await mongoose.connection.close();
-    });
+    }, 10000);
 });
