07193d1ff7892849f2ec0d719126d953bf485fc5
Ben <btmolloy2@gmail.com>
2024-04-18 04:41:25 -0400

    Test Ben

---
+++ __tests__/BenIntegrationTest.bat
@@ -0,0 +1,8 @@
+rem Run Jest tests
+npx jest --runInBand __tests__/brooks-loginLogout.test.js ^
+            __tests__/brooks-sessionVariables.test.js ^
+            __tests__/PasswordEncryption_UnitTest_Bryce_Bien.test.js ^
+            __tests__/Subtasks_UnitTest_Bryce_Bien.test.js ^
+            __tests__/recurringTask.test.js
+
+pause
