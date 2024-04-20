7726c4dc37fbc52e6440a4ad7d21f8c0150c46ef
Cody King <122841869+Cjking57893@users.noreply.github.com>
2024-04-20 19:31:46 -0400

    Fixed tag search test bug

--- __tests__/tagSearch_Quay_.test.js
+++ __tests__/tagSearch_Quay_.test.js
@@ -1,9 +1,22 @@
 const request = require('supertest');
 const { app, server} = require('../index');
 const TodoTask = require('../models/TodoTask');
+const UserData = require('../models/UserData');
 const mongoose = require("mongoose");
+const testUsername = 'test@test.com';
+const testPassword = 'password123!!';
 
 describe('Tag Search Feature', () => {
+  beforeEach(async () => {
+      agent = request.agent(app);
+      await agent.post('/create-account').send({ username: testUsername, password: testPassword });
+      await agent.post('/login').send({ username: testUsername, password: testPassword });
+  });
+
+  afterEach(async () => {
+    await UserData.findOneAndDelete({ username: testUsername });
+  });
+
     // Close MongoDB connection after all tests
     afterAll(async () => {
       await server.close();
@@ -13,24 +26,26 @@ describe('Tag Search Feature', () => {
 
   it('should return tasks associated with the specified tag', async () => {
     // Create some test tasks with different tags
-    const task1 = new TodoTask({ title: 'Task 1', tag: 'Work', user: "ABC"});
-    const task2 = new TodoTask({ title: 'Task 2', tag: 'Personal', user: "ABC"});
-    const task3 = new TodoTask({ title: 'Task 3', tag: 'Work', user: "ABC"});
+    var currentDate = new Date();
+
+    const task1 = new TodoTask({ title: 'Task 1', tag: 'Work', user: testUsername, date: currentDate});
+    const task2 = new TodoTask({ title: 'Task 2', tag: 'Personal', user: testUsername, date: currentDate});
+    const task3 = new TodoTask({ title: 'Task 3', tag: 'Work', user: testUsername, date: currentDate});
     await Promise.all([task1.save(), task2.save(), task3.save()]);
 
     // Perform a search for tasks with the 'Work' tag
-    const response = await request(app) // Makes a request to the Express app
+    const response = await agent // Makes a request to the Express app
   .post('/tag') // Specifies the endpoint to send the POST request to
-  .send({ tag: 'Work', isTest: true}) // Sends a JSON payload with the specified data
-  .set('Content-Type', 'application/json') // Sets the Content-Type header to JSON
-  .set('Accept', 'application/json'); // Sets the Accept header to JSON
-  console.log(response.text);
+  .send({ tag: 'Work'}) // Sends a JSON payload with the specified data
     // Assert that the response contains the expected tasks
     expect(response.status).toBe(200);
     expect(response.text).toContain("Work");
-    expect(response.text).toContain("Task 1");
     expect(response.text).toContain("Task 3");
     expect(response.text).not.toContain("Task 2");
 
+    // Deleting test documents from database
+    await TodoTask.findByIdAndDelete(task1._id);
+    await TodoTask.findByIdAndDelete(task2._id);
+    await TodoTask.findByIdAndDelete(task3._id);
   });
 });
